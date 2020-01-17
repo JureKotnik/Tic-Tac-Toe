b729ba3995a19750a7cc957b69e33e61b93f4157
Jure Kotnik <jure2002k@gmail.com>
2020-01-17 08:33:17 +0100

    first save

---
+++ main.js
@@ -0,0 +1,22 @@
+const { app, BrowserWindow } = require('electron')
+
+require('electron-debug')({
+    showDevTools: process.env.NODE_ENV === 'development'
+})
+
+function createWindow() {
+    let win = new BrowserWindow({
+        width: 400,
+        height: 500,
+        resizable: false,
+        icon: 'icon.pngtic.jpg',
+        webPreferences: {
+            nodeIntegration: true
+        }
+    });
+
+    win.setMenu(null);
+    win.loadFile('test.html')
+}
+
+app.on('ready', createWindow)
